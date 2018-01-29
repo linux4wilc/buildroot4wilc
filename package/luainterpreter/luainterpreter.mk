d63b632168bd770b871298977629c88035a8f685
AdhamAbozaeid <adham.abozaeid@microchip.com>
2018-01-29 11:12:15 -0700

    Instead of keeping a customized version of buildroot, mainline buildroot will be used from git://git.buildroot.net/buildroot tag 2017.08. The patch maintained in this repo should be applied to apply wilc specific configuration to minimize the user's changes.

--- package/luainterpreter/luainterpreter.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-#
-# luainterpreter
-#
-################################################################################
-
-LUAINTERPRETER_ABIVER = $(call qstrip,$(BR2_PACKAGE_LUAINTERPRETER_ABI_VERSION))
-
-$(eval $(virtual-package))
